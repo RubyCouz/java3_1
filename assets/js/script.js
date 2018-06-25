12da73b3ad623f6ba49283a53237daa125d8241b
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-06-25 10:54:58 +0200

    partie3 exo1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function borderColor() {
+  //récupération de l'élément et modification du style
+  document.getElementById('image1').style.border = 'red 3px solid';
+}
+function noBorder() {
+  document.getElementById('image1').style.border = '0';
+}
